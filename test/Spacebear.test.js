99ca2c58fcbd3e920a32efaae0adc265671ec6ed
IvanC <ivan.cavlek1403@gmail.com>
2022-11-04 10:07:35 +0100

    Updated unit test

--- test/Spacebear.test.js
+++ test/Spacebear.test.js
@@ -4,6 +4,6 @@ contract("Spacebear", (accounts) => {
     it("Should credit an NFT to a specific account", async () => {
         const SpacebearInstance = await Spacebear.deployed();
         await SpacebearInstance.safeMint(accounts[1], "spacebear_1.json");
-        console.log(SpacebearInstance.ownerOf(0));
+        assert.equal(await SpacebearInstance.ownerOf(0), accounts[1], "Owner of Token 1 is not equal to account 2");
     })
 })
